99a4b4306e169e8b1dba6810530703c0d8818d52
MatheusHASH <matheusferreirapimenta1@gmail.com>
2022-03-16 16:48:30 -0300

    First commit

---
+++ pages/ForgotPassword/index.js
@@ -0,0 +1,10 @@
+
+const ForgotPass = ()=>{
+  return (
+      <>
+        Recupere sua Senha pelo Email!
+      </>
+  )
+} 
+
+export default ForgotPass
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import { Flex, Input, Heading, Button, useColorMode, useColorModeValue, Link } from "@chakra-ui/react";
+
+const IndexPage = ()=>{
+
+  const { toggleColorMode } = useColorMode()
+  const formBackground = useColorModeValue("gray.100", "gray.700")
+  
+  return (
+      <Flex height="100vh" alignItems="center" justifyContent="center">
+        <Flex direction="column" background={formBackground} p={12} rounded={6}>
+          <Heading mb={6} textAlign="center">Log in</Heading>
+          <Input placeholder="matheus@mail.com" mb={3} type="email" />
+          <Input placeholder="*******"          mb={6} type="password" />
+          <Button colorScheme="blue">Log in</Button>
+          <Link mb={6} href="./ForgotPassword">Forgot Password?</Link>
+          <Button colorScheme="purple" onClick={toggleColorMode}>
+          Change theme
+          </Button>
+        </Flex>
+      </Flex>
+  )
+} 
+
+export default IndexPage
